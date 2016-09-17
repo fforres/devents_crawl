b4fd078ebd63df1b02309e4e0952fdd6fb440f1b
fetorres <fetorres@groupon.com>
2016-09-17 15:06:22 -0300

    update

--- src/index.js
+++ src/index.js
@@ -7,19 +7,22 @@ import { upsertEvents } from './schemas/events';
 
 mongoose.connect(env.mongodb.url);
 
-// GETTING ALL TECH MEETUPS FOR CHILE
-get('/find/groups', {
-  city: 'chile',
-  category: 34,
-})
-.then(upsertGroups)
-.then(retrieveGroups)
-.then((groups) => {
-  const promises = groups.map(group => get(`/${group.meetup_urlname}/events`, {
-    page: 2,
+// GETTING ALL TECH MEETUPS FOR CHILE EVERY 30 minutes
+
+setInterval(() => {
+  get('/find/groups', {
+    city: 'chile',
+    category: 34,
   })
-  .then(events => upsertEvents(events, group._id)));
-  return promises.all;
-})
-.then(console.log)
-.catch(console.log)
+  .then(upsertGroups)
+  .then(retrieveGroups)
+  .then((groups) => {
+    const promises = groups.map(group => get(`/${group.meetup_urlname}/events`, {
+      page: 2,
+    })
+    .then(events => upsertEvents(events, group._id)));
+    return promises.all;
+  })
+  .then(console.log)
+  .catch(console.log);
+}, 1000 * 60 * 30);
